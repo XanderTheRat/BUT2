75f7efd804ddb5f1abf60e1ffadacad7463bb26a
XanderTheRat <saussic0canard@gmail.com>
2025-11-05 14:59:19 +0100

    Add references for borrow Checker and swap()

--- RUST/TP1/ownership/src/main.rs
+++ RUST/TP1/ownership/src/main.rs
@@ -13,7 +13,7 @@ fn main() {
 
     println!("Perimetre pour un rectangle de {} sur {} : {}", my_rectangle.width, my_rectangle.length, perimeter(&my_rectangle));
     let a = perimeter2(my_rectangle);
-    println!("Perimeter2 a: {}", a);
+    println!("Perimetre pour un rectangle de {}, {} avec la deuxieme fonction : {}", my_rectangle.length, my_rectangle.width, a);
 }
 
 fn average(n1: f64, n2: f64) -> f64 {
@@ -32,4 +32,22 @@ fn perimeter(rect : &Rectangle) -> f64 {
 
 fn perimeter2(rect : Rectangle) -> f64 {
     (rect.length+rect.width)*2.0
+}
+
+fn print_reference() {
+    let x = 18;
+
+    let _ref1 = &x;
+    let _ref2 = &x;
+
+    let mut ref3 = &x;
+    let mut ref4 = &x;
+
+    println!("{}, {}", ref3, ref4);
+}
+
+fn swap(nombre1 : &mut f64, nombre2: &mut f64) -> () {
+    let temp = *nombre2;
+    *nombre2 = *nombre1;
+    *nombre1 = temp;
 }
