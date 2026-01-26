f36d905311321a754563187a99529dc55f434bc8
XanderTheRat <saussic0canard@gmail.com>
2026-01-26 09:12:30 +0100

    Initial commit

--- dev_mobile/TP2/app/src/main/java/iut/mobile/tp2/MainActivity.kt
+++ dev_mobile/TP2/app/src/main/java/iut/mobile/tp2/MainActivity.kt
@@ -1,20 +1,107 @@
 package iut.mobile.tp2
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import iut.mobile.tp2.ui.theme.TP2Theme
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContent {
+            TP2Theme {
+                MaterialTheme {
+                    AppNavigation()
+                }
+            }
         }
+    }
+}
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "home") {
+
+        composable(route = "home") {
+            HomeScreen(navController = navController)
+        }
+
+        composable(route = "form") {
+            FormScreen(navController = navController)
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+    text = "Hello $name!",
+        modifier = modifier
+    )
+}
+@Composable
+fun HomeScreen(navController: NavController) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Bienvenue dans ma première application compose navigation",
+            style = MaterialTheme.typography.titleMedium
+        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        Button(
+            onClick = {
+                navController.navigate(route = "form")
+            }
+        ) {
+            Text(text = "Accéder au formulaire")
+        }
+    }
+}
+@Composable
+fun FormScreen(navController: NavController) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Page Formulaire (à construire)")
+        Button(onClick = { navController.popBackStack() }) {
+            Text("Retour")
+        }
+    }
+}
+
+@Composable
+fun GreetingPreview() {
+    TP2Theme {
+        Greeting("Android")
     }
 }
